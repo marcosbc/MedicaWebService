b3dbcd8d8419ddddadf0c6d28109c76db1c4eafa
Marcos Bjoerkelund <marcosbjorkelund@gmail.com>
2017-05-25 01:33:48 +0200

    Eliminar fichero innecesario

--- bin/cliente.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-JAVA=java
-if [ -f medicaclient/MedicaClient.class ]; then
-    $JAVA medicaclient/MedicaClient "$@"
-else
-    echo "Necesitas compilar el cliente: \$ make client"
-    exit 1
-fi
